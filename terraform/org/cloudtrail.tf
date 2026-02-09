27e75296065497328e5c7b0d57dec1e4d3198481
vivekchalla246 <vivekchalla246@gmail.com>
2026-02-09 08:32:09 +0000

    security: harden CloudTrail with KMS, CW logs, validation

--- terraform/org/cloudtrail.tf
+++ terraform/org/cloudtrail.tf
@@ -1,7 +1,55 @@
 resource "aws_cloudtrail" "org_trail" {
   name                          = "organization-trail"
   s3_bucket_name                = aws_s3_bucket.central_logs.id
+
   is_organization_trail         = true
+  is_multi_region_trail         = true
   include_global_service_events = true
   enable_logging                = true
+  enable_log_file_validation    = true
+
+  kms_key_id = aws_kms_key.cloudtrail.arn
+
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_role_arn = aws_iam_role.cloudtrail_logs_role.arn
+}
+
+resource "aws_kms_key" "cloudtrail" {
+  description             = "KMS key for CloudTrail logs"
+  enable_key_rotation     = true
+  deletion_window_in_days = 30
+}
+
+resource "aws_cloudwatch_log_group" "cloudtrail" {
+  name              = "/aws/cloudtrail/organization"
+  retention_in_days = 90
+}
+
+resource "aws_iam_role" "cloudtrail_logs_role" {
+  name = "cloudtrail-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "cloudtrail.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "cloudtrail_logs_policy" {
+  role = aws_iam_role.cloudtrail_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
 }
